23997c22fde852903e2c5c96b27b24ac4dcbc312
Gustavo <gustavo.shigueo@proton.me>
2023-08-07 14:44:34 -0300

    Add argument reexports

--- src/captcha_arguments/mod.rs
+++ src/captcha_arguments/mod.rs
@@ -3,8 +3,13 @@
 //!
 //! # Usage notes:
 //!
+//! * All implementers of the [`arguments::CaptchaArguments`] trait
+//! are re-exported by the top level of this module.
 //! * All fields not typed as [`Option::<T>`] are required by the 2captcha
 //! API and should not be filled by the [`Default`] trait
+//! * Special parameter types like [`proxy_type::ProxyType`] are not re-exported
+//! here
+
 pub mod character_restrictions;
 pub mod language;
 pub mod proxy_type;
@@ -18,3 +23,12 @@ pub mod normal_captcha;
 pub mod recaptcha_v2;
 pub mod recaptcha_v3;
 pub mod text_captcha;
+
+pub use capy_captcha::CapyCaptcha;
+pub use geetest::Geetest;
+pub use h_captcha::HCaptcha;
+pub use key_captcha::KeyCaptcha;
+pub use normal_captcha::NormalCaptcha;
+pub use recaptcha_v2::RecaptchaV2;
+pub use recaptcha_v3::RecaptchaV3;
+pub use text_captcha::TextCaptcha;
