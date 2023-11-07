d5bcf09bbbf1ea90997b88fe3ba94f4b17a38955
Gustavo <gustavo.shigueo@proton.me>
2023-11-07 16:38:25 -0300

    Reduce amount of public modules in recaptcha

--- src/captcha_types/recaptcha/mod.rs
+++ src/captcha_types/recaptcha/mod.rs
@@ -1,7 +1,7 @@
-pub mod recaptcha_v2;
-pub mod recaptcha_v2_enterprise;
-pub mod recaptcha_v3;
-pub mod solution;
+mod recaptcha_v2;
+mod recaptcha_v2_enterprise;
+mod recaptcha_v3;
+mod solution;
 
 pub use recaptcha_v2::RecaptchaV2;
 pub use recaptcha_v2_enterprise::RecaptchaV2Enterprise;
